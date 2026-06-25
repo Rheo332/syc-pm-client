6d9fd89b1038a3e3329568b461ffbd831e42ebea
rhe <qbbqbbb@gmail.com>
2026-06-26 01:53:28 +0200

    Removed Title Bar background; Added default window size and start location

--- syc-pm-client/MainWindow.xaml.cs
+++ syc-pm-client/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using syc_pm_client.Services.Interfaces;
 using syc_pm_client.Views;
+using WinRT.Interop;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -15,6 +18,21 @@ namespace syc_pm_client
         public MainWindow(INavigationService nav)
         {
             InitializeComponent();
+
+            ExtendsContentIntoTitleBar = true;
+            SetTitleBar(MainFrame);
+
+            var hWnd = WindowNative.GetWindowHandle(this);
+            var windowId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            var appWindow = AppWindow.GetFromWindowId(windowId);
+            appWindow.Resize(new Windows.Graphics.SizeInt32(1200, 700));
+
+            var displayArea = DisplayArea.GetFromWindowId(windowId, DisplayAreaFallback.Primary);
+            var centerPosition = new Windows.Graphics.PointInt32(
+                (displayArea.WorkArea.Width - appWindow.Size.Width) / 2,
+                (displayArea.WorkArea.Height - appWindow.Size.Height) / 2
+            );
+            appWindow.Move(centerPosition);
 
             nav.Initialize(MainFrame);
             nav.Navigate<LoginPage>();
